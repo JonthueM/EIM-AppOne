79c0f411918afd43c5defb8b5f9e830905a284f8
JonthueM <admin@jonthuemichel.com>
2022-11-08 20:52:12 -0500

    Update

---
+++ eim_one/lib/templates/data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+var data =[
+    {
+        'dayTitle': 'The Title',
+        'dayQuote': 'The Quote for today',
+
+        'dayDevoTitle':' Devotional',
+        'dayDevoVideo': ' some url',
+        'dayDevoActivity': 'some activity',
+        'dayDevoReads':['Read One', 'Read Two', 'Read Three'],
+
+        'dayPhysiTitle': 'Physical',
+        'dayPhysiVideo': ' some url',
+        'dayPhysiActivity': 'some activity',
+        'dayPhysiReads':['Read One', 'Read Two', 'Read Three'],
+        
+        'dayPractTitle': 'Physical',
+        'dayPractiVideo': ' some url',
+        'dayPractiActivity': 'some activity',
+        'dayPractiReads':['Read One', 'Read Two', 'Read Three'],
+    },
+]
